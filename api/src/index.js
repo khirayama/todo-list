2fce8f1221a2132cc06341c11a7dc6178b0542a6
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-03-29 06:39:24 +0900

    Implement api server

---
+++ api/src/index.js
@@ -0,0 +1,80 @@
+const http = require('http');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const jwt = require('jwt-simple');
+
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || '127.0.0.1';
+
+const app = express();
+const server = http.createServer(app);
+
+const config = {
+  secret: 'This is secret',
+};
+
+function tokenForUser(user) {
+  return jwt.encode({
+    sub: user._id,
+    iat: new Date().getTime(),
+  }, config.secret);
+}
+
+// model
+const validateEmail = email => (/\S+@\S+.\S+/).test(email);
+
+const User = mongoose.model('user', new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    required: 'Email address is required',
+    validate: [validateEmail, 'Please enter a valid email'],
+  },
+  password: {type: String},
+}));
+
+// index
+mongoose.connect('mongodb://localhost:todolist/todolist');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/v1', new express.Router()
+  .post('/signup', (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    if (!email || !password) {
+      return res.status(422).send({error: 'You must provide email and password'});
+    }
+
+    User.findOne({email}, (err, existingUser) => {
+      if (err) {
+        return next(err);
+      }
+
+      if (existingUser) {
+        return res.status(422).send({error: 'Email is in use.'});
+      }
+
+      const user = new User({email, password});
+
+      user.save(err_ => {
+        if (err_) {
+          if (err_.errors && err_.errors.email) {
+            return res.status(422).send({error: err_.errors.email.message});
+          }
+          return next(err);
+        }
+
+        res.json({token: tokenForUser(user), userId: user._id});
+      });
+    });
+  })
+);
+
+console.log(`listening on http://${host}:${port}`);
+server.listen(port, host);
