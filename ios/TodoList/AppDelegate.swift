61bc6c0e844faea13487ac7aaf0526f5030d9dd3
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-04-02 22:30:05 +0900

    Implement to distribute function in AppDelegate

--- ios/TodoList/AppDelegate.swift
+++ ios/TodoList/AppDelegate.swift
@@ -15,7 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
+    let defaults = UserDefaults.standard
+    let token = defaults.value(forKey: "jwtToken")
+    
+    let navigationController = window!.rootViewController! as! UINavigationController
+    
+    navigationController.performSegue(withIdentifier: token == nil ? "showLoginViewController" : "showTodosViewController", sender: nil)
+    
     return true
   }
 
