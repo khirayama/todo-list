3ec85de1f3894bf17237a95c3ecd537057a0dad6
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-04-05 07:22:30 +0900

    Add NewTodoViewController file

---
+++ ios/TodoList/NewTodoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NewTodoViewController.swift
+//  TodoList
+//
+//  Created by Kotaro Hirayama on 2017/04/05.
+//  Copyright © 2017 Kotaro Hirayama. All rights reserved.
+//
+
+import UIKit
+
+class NewTodoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
